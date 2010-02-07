f8a0a7aa731732ad01334f70560c297ff6c222cd
ever.zet <ever.zet@gmail.com>
2010-02-07 11:03:28 +0200

    Added $SF_SCRIPT_PATH environment variable for adding custom symfony CLI script path to CLI GUI

--- Support/Navigator/navigator.php
+++ Support/Navigator/navigator.php
@@ -5,10 +5,16 @@ $args = $_SERVER['argv'];
 function getSymfonyRunner()
 {
   $projectPath = getenv('TM_PROJECT_DIRECTORY');
+  $symfonyPath = getenv('SF_SCRIPT_PATH');
 
   if ($projectPath && is_file($projectPath . '/symfony'))
   {
     return $projectPath . '/symfony ';
+  }
+
+  if ($symfonyPath)
+  {
+    return $symfonyPath . ' ';
   }
 
   return 'symfony ';
